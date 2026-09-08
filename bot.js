c1ebbaae4b167a38c4bcd11e09e91713ae687802
Oluwafunmito Adejuwon <72803953+a-v-o@users.noreply.github.com>
2026-09-08 22:34:41 +0100

    Refactor queryJobs to destructure response

--- bot.js
+++ bot.js
@@ -179,7 +179,7 @@ bot.command("unschedule", async (ctx) => {
     return;
   }
 
-  const jobs = await agenda.queryJobs({
+  const { jobs, total } = await agenda.queryJobs({
     name: "mint",
     "data.chatId": chatId,
     "data.slug": slug,
